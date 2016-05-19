7720f90d542a5ad2755018e6c6a4264f8a37d204
pablo <pablo@ottaviano>
2016-05-18 23:08:21 -0300

    ohh yeaaa

---
+++ tpdatos/Bnodo.cpp
@@ -0,0 +1,21 @@
+/*
+ * Bnodo.cpp
+ *
+ *  Created on: 18 de may. de 2016
+ *      Author: pablo
+ */
+
+#include "Bnodo.h"
+
+namespace tpdatos {
+
+Bnodo::Bnodo() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Bnodo::~Bnodo() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace tpdatos */
---
+++ tpdatos/Bnodo.h
@@ -0,0 +1,21 @@
+/*
+ * Bnodo.h
+ *
+ *  Created on: 18 de may. de 2016
+ *      Author: pablo
+ */
+
+#ifndef BNODO_H_
+#define BNODO_H_
+
+namespace tpdatos {
+
+class Bnodo {
+public:
+	Bnodo();
+	virtual ~Bnodo();
+};
+
+} /* namespace tpdatos */
+
+#endif /* BNODO_H_ */
